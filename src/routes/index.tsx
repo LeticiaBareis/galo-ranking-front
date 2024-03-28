cde3d4b5a7ae70e1a519e475d57f693fbb439a13
LeticiaBareis <reis.leticiabarroso@gmail.com>
2024-03-28 00:26:58 -0300

    Feat: Adicionando materialUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () =>{
   return(
     <Routes>
-      <Route path="/pg-inicial" element={<p> Página Inicial</p>}/>
+      <Route path="/pg-inicial" element={<Button>Teste</Button>}/>
       <Route path="*" element ={<Navigate to="/pg-inicial"/>}/>
 
     </Routes>
